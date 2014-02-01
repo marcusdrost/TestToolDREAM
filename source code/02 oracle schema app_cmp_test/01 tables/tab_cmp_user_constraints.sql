65ebf8f766fcee58737656d5570e392237a7d56a
marcusdrost <marcus@drost.name>
2014-02-01 11:58:47 +0100

    Delete tab_cmp_user_constraints.sql

--- source code/02 oracle schema app_cmp_test/01 tables/tab_cmp_user_constraints.sql
+++
@@ -1,9 +0,0 @@
---------------------------------------------------------
---  File created - Friday-January-31-2014   
---------------------------------------------------------
---------------------------------------------------------
---  DDL for Table CMP_USER_CONSTRAINTS
---------------------------------------------------------
-
-  CREATE TABLE "CMP_USER_CONSTRAINTS" ("TAB" VARCHAR2(30 CHAR), "COL" VARCHAR2(30 CHAR), "UNQ_CONS" NUMBER(1,0), "TEC_UNQ_CONS" NUMBER(1,0), "FK_CONS" NUMBER(1,0), "FK_TAB" VARCHAR2(30 CHAR), "FK_COL" VARCHAR2(30 CHAR), "EXCLUDE_COL" NUMBER(1,0), "NON_STND_TEC_COL" NUMBER(1,0), "LEFT_COL_IND" NUMBER(1,0), "RIGHT_COL_IND" NUMBER(1,0), "RUN_ID" VARCHAR2(200 CHAR))
-/
