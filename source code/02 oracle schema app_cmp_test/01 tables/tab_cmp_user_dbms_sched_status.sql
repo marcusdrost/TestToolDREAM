176efb82fdfea3e9ce531eb3bb7aa662373f4f22
marcusdrost <marcus@drost.name>
2014-02-01 11:59:12 +0100

    Delete tab_cmp_user_dbms_sched_status.sql

--- source code/02 oracle schema app_cmp_test/01 tables/tab_cmp_user_dbms_sched_status.sql
+++
@@ -1,9 +0,0 @@
---------------------------------------------------------
---  File created - Friday-January-31-2014   
---------------------------------------------------------
---------------------------------------------------------
---  DDL for Table CMP_USER_DBMS_SCHED_STATUS
---------------------------------------------------------
-
-  CREATE TABLE "CMP_USER_DBMS_SCHED_STATUS" ("STATUS" VARCHAR2(20), "ERR_MSG" VARCHAR2(2000), "JOB_NAME" VARCHAR2(2000), "LOG_DATE" DATE)
-/
