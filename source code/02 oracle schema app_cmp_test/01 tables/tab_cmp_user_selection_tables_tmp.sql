7a3add3889e04594efe513930f8293342a3e0dee
marcusdrost <marcus@drost.name>
2014-02-01 12:02:46 +0100

    Delete tab_cmp_user_selection_tables_tmp.sql

--- source code/02 oracle schema app_cmp_test/01 tables/tab_cmp_user_selection_tables_tmp.sql
+++
@@ -1,9 +0,0 @@
---------------------------------------------------------
---  File created - Friday-January-31-2014   
---------------------------------------------------------
---------------------------------------------------------
---  DDL for Table CMP_USER_SELECTION_TABLES_TMP
---------------------------------------------------------
-
-  CREATE TABLE "CMP_USER_SELECTION_TABLES_TMP" ("TAB" VARCHAR2(100 CHAR), "IN_SCOPE" NUMBER(5,0) DEFAULT 0, "ORDER_ASCENDING" NUMBER(6,0) DEFAULT 100, "RUN_ID" VARCHAR2(200 CHAR))
-/
